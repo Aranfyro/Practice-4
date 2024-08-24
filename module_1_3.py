b6b80cdb64e4f7f902831a1ef345e795e688dbf1
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-08-24 09:50:39 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+
+name = 'Artem'
+print ('Name:' + name)
+age = 22
+print ('Age: ' + str(age))
+New_age = age + 3
+print ('New Age: ' + str(New_age))
+is_student = True
+print ('Is Student: ' + str(is_student))
+
